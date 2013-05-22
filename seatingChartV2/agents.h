c2565ece8a05fdbc0731c732f5c650f36e9daa89
Jason H <fallnomega@gmail.com>
2013-05-22 13:39:20 -0700

    rearranged and added
    moved old files to version 1, added version two on to the repo.

---
+++ seatingChartV2/agents.h
@@ -0,0 +1,51 @@
+#ifndef AGENTS_H
+#define AGENTS_H
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <fstream>
+using std::ofstream;
+using namespace std;
+class Agent
+{
+protected:
+  string uuid;
+  string name;
+  string om;
+  string location;
+  string desktop;
+  string monitor;
+public:
+  Agent();
+  Agent(string uuid1,string name1, string om1, string location1, string desktop1,string monitor1):
+    uuid(uuid1),
+    name(name1),
+    om(om1),
+    location(location1),
+    desktop(desktop1),
+    monitor(monitor1)
+    {};
+  Agent(const char* const token[6]):location(token[0]),name(token[1]),uuid(token[2]),om(token[3]),desktop(token[4]),monitor(token[5]){};
+  friend void printToFile(vector<const Agent*> agents);
+  friend vector<const Agent*> deleteAgent(vector<const Agent*> agents);
+  friend vector<const Agent*> editAgent(vector<const Agent*> agents);
+  string setName();
+  void print() const;
+  ~Agent(){};
+
+};
+
+
+//class Agent_Desk
+//{
+//protected:
+//
+//public:
+//  ~Agent_Desk(){};
+//  void print() const;
+//};
+
+
+
+#endif
---
+++ seatingChartV2/seatingchart.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include "agents.h"
+using namespace std;
+
+
+
+int menu();
+ostream& operator<<(ofstream, vector<const Agent*>);
+ostream& operator<<(ostream& out, const Agent* agent);
+//vector<const Agent*> text(vector<const Agent*> agents);
+void printToFile(const vector<const Agent*>);
+vector<const Agent*> dat(vector<const Agent*> agents);
+vector<const Agent*> addAgent(vector<const Agent*> agents);
+vector<const Agent*> deleteAgent(vector<Agent*> agents);
+vector<const Agent*> text(vector<const Agent*> agents);
+vector<const Agent*> editAgent(vector<Agent*> agents);
+
+int main()
+{
+  vector<const Agent*> agents; 
+  cout << "Seating Chart\n-------------\n\n";
+  while (true)
+  {
+    int choice = menu();
+    if(choice==1)agents =dat(agents);//if choice 1
+    if(choice==2) printToFile(agents);//if choice 2
+    if(choice==3)
+    {
+      vector<const Agent*>::iterator it;
+      for(it=agents.begin();it!=agents.end();it++)cout << *it;
+    }//if choice 3
+    if(choice==4) agents = addAgent(agents); //if choice = 4
+    if(choice==5) agents = deleteAgent(agents);//if choice = 5
+    if(choice==6) agents = editAgent(agents);//if choice = 6
+    if(choice==99) break;//if choice 99
+    if(choice==9988) agents =text(agents);
+  }//while
+}//main
+
+/////////////
+//functions//
+/////////////
+
+
+
+
+
